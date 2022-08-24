ad16ca78343d676f5c912f90be75896d9ca7366e
zedoppa <61616360+zedoppa@users.noreply.github.com>
2022-08-24 12:31:10 +0800

    Added minimize function through taskbar-clicked.

--- MySQL8 Launcher/Form1.cs
+++ MySQL8 Launcher/Form1.cs
@@ -190,6 +190,21 @@ namespace MySQL8_Launcher
         }
         #endregion
 
+        #region Minimize By Taskbar Clicked
+        const int WS_MINIMIZEBOX = 0x20000;
+        const int CS_DBLCLKS = 0x8;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.Style |= WS_MINIMIZEBOX;
+                cp.ClassStyle |= CS_DBLCLKS;
+                return cp;
+            }
+        }
+        #endregion
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && this.components != null)
